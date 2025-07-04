0dd28f450f58dec35f072ccb44eff8e5b07da01a
Luhan Mairink <luhan.mairink@sys10.com.br>
2025-07-04 13:22:31 -0300

    feat: configuração de plugins e dependências

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,17 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
+const {
+  wrapWithReanimatedMetroConfig,
+} = require("react-native-reanimated/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
 // Configuração para SVGs
-config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
-config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
+config.transformer.babelTransformerPath = require.resolve(
+  "react-native-svg-transformer"
+);
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  (ext) => ext !== "svg"
+);
+config.resolver.sourceExts = [...config.resolver.sourceExts, "svg"];
 
-module.exports = config; 
+module.exports = wrapWithReanimatedMetroConfig(config);
