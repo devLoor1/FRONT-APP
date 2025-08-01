56c174bf185a74bd39b5aaeee801995ec4d5f686
Caio Henrique Galli dos Santos <caio@sys10.com.br>
2025-08-01 17:29:23 -0300

    Fix version

--- src/helpers/version/version.tsx
+++ src/helpers/version/version.tsx
@@ -1,9 +1,13 @@
 import Constants from 'expo-constants';
 import * as Updates from 'expo-updates';
+import { useUpdates } from "expo-updates";
 
 export default function Version(): string {
+  const { lastCheckForUpdateTimeSinceRestart } = useUpdates();
+  
   return [
     Updates.channel,
+    Updates.updateId?.substring(0, 8),
     Constants.expoConfig?.extra?.env?.env,
     Constants.expoConfig?.extra?.version,
   ].join(' - ');
